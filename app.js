c501a5a811e401298f4383a9cfab72544d7349b2
amontalvof <amontalvof92@gmail.com>
2021-04-05 21:19:08 -0500

    feat: render a complete website

--- app.js
+++ app.js
@@ -4,8 +4,11 @@ const port = 8080;
 
 app.use(express.static('public'));
 
-app.get('/hello-world', (req, res) => {
-    res.send('Hello World');
+app.get('/generic', (req, res) => {
+    res.sendFile(__dirname + '/public/generic.html');
+});
+app.get('/elements', (req, res) => {
+    res.sendFile(__dirname + '/public/elements.html');
 });
 app.get('*', (req, res) => {
     res.sendFile(__dirname + '/public/404.html');
