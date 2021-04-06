8557c7a92b42fdd77ab19cc13c3d4bda00ae57b5
amontalvof <amontalvof92@gmail.com>
2021-04-06 17:46:46 -0500

    feat: handlebars

--- app.js
+++ app.js
@@ -2,8 +2,13 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
+app.set('view engine', 'hbs');
+
 app.use(express.static('public'));
 
+app.get('/', (req, res) => {
+    res.render('home');
+});
 app.get('/generic', (req, res) => {
     res.sendFile(__dirname + '/public/generic.html');
 });
@@ -11,7 +16,8 @@ app.get('/elements', (req, res) => {
     res.sendFile(__dirname + '/public/elements.html');
 });
 app.get('*', (req, res) => {
-    res.sendFile(__dirname + '/public/404.html');
+    // res.sendFile(__dirname + '/public/404.html');
+    res.send('404 Not Found');
 });
 
 app.listen(port, () => {
