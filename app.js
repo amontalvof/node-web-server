055da1525822064852035cb5e0556d1660476494
amontalvof <amontalvof92@gmail.com>
2021-04-07 17:52:56 -0500

    feat: arguments from controller

--- app.js
+++ app.js
@@ -7,7 +7,10 @@ app.set('view engine', 'hbs');
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.render('home');
+    res.render('home',{
+        name: 'Andy Montalvo',
+        title: 'Node Course'
+    });
 });
 app.get('/generic', (req, res) => {
     res.sendFile(__dirname + '/public/generic.html');
